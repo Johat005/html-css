e2800479d76dbdd44d21fba9f8ec5e5ffa28f143
SoyJoha <mayerli.tellez.99@gmail.com>
2026-06-24 16:17:04 -0500

    Creando repositorio en GitHub

---
+++ JS Basico/codigo.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Dime tu nombre");
+alert("Hola " + nombre)
+
+let number1= "53";
+let number2= 8;
+frase = number1.concat(number2)
+alert(frase)
+
+document.write(number1 === number2)
